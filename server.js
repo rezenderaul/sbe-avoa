01dddeccc4224ddd8415bf1ae248d2462d7aa620
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2024-05-04 11:29:08 -0300

    created class App

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middleware();
+    this.routes();
+  }
+
+  middleware() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {}
+}
+
+export default new App().app;
